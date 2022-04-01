fixed free energy calculation

FENEX/functions.py
@@ -123,7 +123,7 @@ def f_first_point(f2new: float,free_energy:np.ndarray,z:np.ndarray,cov:np.ndarra
     fun   = (free_energy+coef[0,:] +
              coef[1,:]-0.5*(coef[2,:] +
              coef[3,:]) - coef[4,:])
-    return fun[0] - fun[1] #question
+    return fun[0] - fun[1] 
 
 def df_first_point(f2new: float,free_energy:np.ndarray,z: np.ndarray,cov: np.ndarray,f1new: float,f: np.ndarray) -> float:
 
@@ -208,7 +208,8 @@ def refine_coex(free_energy:np.ndarray,z: np.ndarray,cov: np.ndarray,f: np.ndarr
         zsat[1,i,:] = z[1,i,:] -cov[1,i,:]*df[1,:] - cov[2,i,:]*df[0,:]
         coef=poly_coefficients(df,zsat[:,i,:],cov[:,i,:])
         #redundancy 
-        free_energy_zsat[i]   = (free_energy[i,0]+coef[0,:] +
+     
+        free_energy_zsat[i,:]   = (free_energy[i,:]+coef[0,:] +
                  coef[1,:]-0.5*(coef[2,:] +
                  coef[3,:]) - coef[4,:])
         
@@ -264,6 +265,7 @@ def cal_free_energy(f_a: np.ndarray,f_b: np.ndarray,z_a: np.ndarray,z_b: np.ndar
     free_energy_b = (free_energy_a+0.5*(coef_a[0,:]+coef_b[0,:]+
     coef_a[1,:]+coef_b[1,:])+(coef_b[2,:]-coef_a[2,:]+coef_b[3,:]-coef_a[3,:])/12
     +(coef_b[4,:]-coef_a[4,:])/6) 
+    
     return free_energy_b
 
 def calculate_first_point(f1new: float,f: np.ndarray,free_energy: np.ndarray, z: np.ndarray, cov: np.ndarray) -> float:
@@ -549,7 +551,7 @@ def estimate_coexistence(int_type:int ,f1new: float,f: np.ndarray,free_energy: n
             bpt = ipt +1
             free_energy[bpt,:] = (cal_free_energy(f[:,ipt,:],f[:,bpt,:],
             z[:,ipt,:],z[:,bpt,:],cov[:,ipt,:],cov[:,bpt,:],free_energy[ipt,:]))
-
+        print(free_energy[bpt,:])
         apt = Npoints - 2
         bpt = Npoints - 1
         f2new0 = first_guess_newton(free_energy[bpt,:],z[:,bpt,:],f1new,f[:,bpt,:])

FENEX/tests/test_FENEX.py
@@ -94,15 +94,26 @@ def test_fenex():
     cov = np.array([[[0.96084424,0.99139901],[0.89934423,0.93119316]],[[0.04958482,0.06373951],[0.04330027,0.06763215]],[[0.07901728,0.13309644],[0.07107514,0.12661185]]])
     stats = np.array([[[0.37352467,0.224034],[0.366731,0.22013267]],[[-2.08372932,-1.57569717],[-1.95799492,-1.48165106]]])
     free_energy,f2new = estimate_coexistence(1 ,f1new,f,free_energy, z, cov) 
+    free_energy_check=[[0.0349  ,   0.        ],[0.3923189 , 0.36023006]]
     f2check=8.37242894118646
+    delta =6e-4
+    # assert function() to check if values are almost equal
+    assert_almost_equal(f2new, f2check,decimal=delta)
+    assert_almost_equal(free_energy, free_energy_check,decimal=delta)
+    
+    
+def test_refine_coex():
+    free_energy=np.array([[0.0349  ,   0.        ],[0.3923189 , 0.36023006]])
+    f = np.array([[[1.15,1.15],[1.1,1.1]],[[8.02277426,8.02277426],[8.13763783,8.13763783]]])
+    z = np.array([[[-2.08372932,-1.57569717],[-1.95799492,-1.48165106]],[[2.23293245,2.47249644],[2.23128398,2.46899051]]])
+    cov = np.array([[[0.96084424,0.99139901],[0.89934423,0.93119316]],[[0.04958482,0.06373951],[0.04330027,0.06763215]],[[0.07901728,0.13309644],[0.07107514,0.12661185]]])
+    stats = np.array([[[0.37352467,0.224034],[0.366731,0.22013267]],[[-2.08372932,-1.57569717],[-1.95799492,-1.48165106]]])
     results_sat = refine_coex(free_energy,z,cov,f,stats[1,:,:])
     delta =6e-4
     f2sat_check = [8.1685 ,8.27357712677913]
     zsat_check =[[[-.2095241E+01 ,-.1595087E+01], [ -.1967657E+01 ,-.1498863E+01]],
- [[ 0.2225709E+01 ,0.2463211E+01], [ 0.2225398E+01 ,0.2459797E+01]]]
-    # assert function() to check if values are almost equal
-    assert_almost_equal(f2new, f2check,decimal=delta)
+    [[ 0.2225709E+01 ,0.2463211E+01], [ 0.2225398E+01 ,0.2459797E+01]]]
+    free_energysat_check = [[0.36001651, 0.35971349],[0.69443783, 0.69398819]]
     assert_almost_equal(results_sat['f2_sat'], f2sat_check,decimal=delta)
     assert_almost_equal(results_sat['z_sat'],zsat_check,decimal=delta)
-    
-    
+    assert_almost_equal(results_sat['free_energy_sat'],free_energysat_check,decimal=delta)